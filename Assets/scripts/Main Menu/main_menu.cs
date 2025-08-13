574c237723c03107501ecd0fcac43e81260e428e
Pingu-inium <20pranav06@gmail.com>
2025-08-13 17:03:35 +0100

    redid the menu
    redid the menu

--- Assets/scripts/Main Menu/main_menu.cs
+++ Assets/scripts/Main Menu/main_menu.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
 
 public class main_menu : MonoBehaviour
 {
    public void play_game ()
    {
     SceneManager.LoadScene("game_test");
+   }
+
+   public void quit_game ()
+   {
+    Debug.Log("application closed");
+    Application.Quit();
    }
 }
